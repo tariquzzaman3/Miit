1893350b59c2bffef65d4ca1a8fbcd1020ec8c32
Tariq <45267425+tariquzzaman3@users.noreply.github.com>
2026-08-25 03:35:36 +0600

    Fix ExperimentalMaterial3Api compile error

--- app/src/main/java/com/miit/app/MainActivity.kt
+++ app/src/main/java/com/miit/app/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -58,6 +59,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MiitApp() {
     val nav = rememberNavController()
