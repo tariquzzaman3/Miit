f54d7f48114b9d6d8a69fee0211f00eb0f472b55
Tariq <45267425+tariquzzaman3@users.noreply.github.com>
2026-09-03 04:05:25 +0600

    Fix Compose weight import for clean build

--- miit/app/core/MainActivity.kt
+++ miit/app/core/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.layout.fillMaxSize
