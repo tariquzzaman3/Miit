19e401b985f4466e1d064de598d6dcf5e2cae9f1
Tariq <45267425+tariquzzaman3@users.noreply.github.com>
2026-09-03 17:50:44 +0600

    Fix editor scrolling import

--- miit/app/core/MiitWatchFaceEditor.kt
+++ miit/app/core/MiitWatchFaceEditor.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
