1f3fcc82b94abf6d88417c486c76c6ce5ad54588
Tariq <45267425+tariquzzaman3@users.noreply.github.com>
2026-09-05 15:51:52 +0600

    Prepare functional editor text controls

--- miit/app/core/MiitWatchFaceEditor.kt
+++ miit/app/core/MiitWatchFaceEditor.kt
@@ -19,6 +19,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
