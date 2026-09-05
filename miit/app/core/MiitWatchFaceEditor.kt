249f1f8637477563911152c80c2e0ae964814f52
Tariq <45267425+tariquzzaman3@users.noreply.github.com>
2026-09-05 15:53:11 +0600

    Prepare editor for actual preview rendering

--- miit/app/core/MiitWatchFaceEditor.kt
+++ miit/app/core/MiitWatchFaceEditor.kt
@@ -2,6 +2,7 @@ package com.miit.app
 
 import android.graphics.BitmapFactory
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.clickable
@@ -35,6 +36,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
