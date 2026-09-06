ca2cf7d043b75be02329b6a3f49718df45d134ae
Tariq <45267425+tariquzzaman3@users.noreply.github.com>
2026-09-07 00:05:29 +0600

    Import system theme detection for MIIT settings

--- miit/app/core/MainActivity.kt
+++ miit/app/core/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
